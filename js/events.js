ff84880c6a39f9812847b7e82c1f80c2a7d046f6
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-30 04:25:48 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -18,7 +18,7 @@ function pressIt(){
   });
 }
 function submitIt(){
-  $("submit").on("submit", function(){
+  $("button").on("submit", function(){
     alert("Your form is going to be submitted now.")
   });
 }
