241fb7f493475c7cae8f0a5f6d51aaa53c1ffbf7
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-30 04:26:53 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -11,7 +11,7 @@ function frameIt(){
   });
 }
 function pressIt(){
-  $("input").on("keydown", function(key){
+  $("form").on("keydown", function(key){
     
     if(key.which === 71){
     alert("G was pushed")}
