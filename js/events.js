8784898d89db94fd59ba03ff0bccb61ec1604bd5
Zachary Jeffreys <zachary.jeffreys@wsu.edu>
2019-01-30 04:23:06 +0000

    Automatically backed up by Learn

--- js/events.js
+++ js/events.js
@@ -1,7 +1,28 @@
-//define functions here
+//define func"tions here
+function getIt(){
+  $("p").on("click", function(){
+    alert("Hey!")
+  });
+}
+
+function frameIt(){
+  $("img").on("load", function(){
+    $("img").addClass("tasty");
+  });
+}
+function pressIt(){
+  $("input").on("keydown", function(){
+    
+    if(keydown.which === 71){
+    alert("G was pushed")}
+  })
+}
+
 
 $(document).ready(function(){
 
 // call functions here
-
+getIt();
+frameIt();
+pressIt();
 });
